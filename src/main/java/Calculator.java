d62a297f36ad5460fde28fd8eb99bfabee28a208
jbwitt <jbwitt11@gmail.com>
2021-10-13 14:53:46 -0600

    implement create unique id

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,3 +1,4 @@
+import java.util.UUID;
 
 class Calculator {
 
@@ -51,7 +52,8 @@ class Calculator {
     if int a = 16 then this method returns: 10000
      */
     String intToBinaryNumber(int n){
-        return null;
+
+      return null;
     }
 
     /*
@@ -63,8 +65,7 @@ class Calculator {
     if you run this function twice with the same String input, it must return 2 unique String IDs
      */
     String createUniqueID(String n){
-
-        return null;
+      return n + UUID.randomUUID().toString();
     }
 
 
